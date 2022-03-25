65317a49316f7ee2651b50e9d9f15d5d823a2fea
Simone Montali <sim.montali@gmail.com>
2022-03-25 14:03:41 +0100

    Things work but kill my RAM

--- text_vectorizer.py
+++ text_vectorizer.py
@@ -106,7 +106,6 @@ class TextVectorizer:
         # create a set containing words from the documents in a given data split
         words = {word for column in columns for sentence in dataset[column] for word in sentence}
         oov_words = words - self.vocabulary.keys()
-        print(oov_words)
         # add the OOV words to the vocabulary giving them a random encoding
         for word in oov_words:
             self.vocabulary[word] = np.random.uniform(-1, 1, size=self.embedding_dim)
@@ -124,10 +123,12 @@ class TextVectorizer:
         -------
         Array of shape (number of documents, number of words, embedding dimension)
         """
-        result = dataset.copy()
         for column in columns:
-            result[column] = result[column].apply(self._transform_document)
-        return result
+            # group-apply-combine approach to avoid filling the RAM
+            for name, group in dataset[column].groupby(level=0):
+                group = group.apply(self._transform_document)
+                dataset[column][name] = group.values.tolist()
+        return dataset
 
     def _transform_document(self, document):
         """
@@ -142,152 +143,31 @@ class TextVectorizer:
         Numpy array of shape (number of words, embedding dimension)
         """
         try:
-            return np.array([self.vocabulary[word] for word in document])
+            if len(document) > self.max_size:
+                return None
+            return np.vstack((np.array([self.vocabulary[word] for word in document]),np.zeros((self.max_size - len(document), self.embedding_dim))))
         except KeyError:
             raise NotAdaptedError(
                 f"The whole document is not in the vocabulary. Please adapt the vocabulary first."
             )
 
 
-class TargetVectorizer:
-    def __init__(self, max_size):
-        """
-        This class one-hot encodes the target documents, containing the POS tags.
-        """
-        self.vectorizer = LabelBinarizer()
-        self.max_size = max_size
-
-    def adapt(self, targets):
-        """
-        Fits the vectorizer for the classes.
-
-        Parameters
-        ----------
-        targets : The target tags for the dataset split given (it is a list of lists).
-        """
-        self.vectorizer.fit(
-            [target for doc_targets in targets for target in doc_targets]
-        )
-
-    def get_classes(self):
-        return self.vectorizer.classes_
-
-    def transform(self, targets):
-        """
-        Performs the one-hot encoding for the dataset Ys, returning a list of encoded document tags.
-
-        Parameters
-        ----------
-        targets : The target tags for the dataset split given (it is a list of lists).
-
-        Returns
-        -------
-        List of shape (number of documents, numbero of tokens, number of classes)
-        """
-        if self.vectorizer.classes_.shape[0] == 0:
-            raise NotAdaptedError(
-                "The target vectorizer has not been adapted yet. Please adapt it first."
-            )
-
-        result = []
-        for document in targets:
-            transformed = self.vectorizer.transform(document)
-            pad_size = (self.max_size - len(document), transformed.shape[1])
-            result.append(
-                np.vstack((self.vectorizer.transform(document), np.zeros(pad_size)))
-            )
-        return np.array(result)
-
-    def inverse_transform(self, targets):
-        """
-        Performs the inverse one-hot encoding for the dataset Ys, returning a list of decoded document tags.
-        Parameters
-        ----------
-        targets : The target tags for the dataset split given, already in one-hot encoding form.
-        Returns
-        -------
-        List of decoded document tags
-        """
-        if self.vectorizer.classes_.shape[0] == 0:
-            raise NotAdaptedError(
-                "The target vectorizer has not been adapted yet. Please adapt it first."
-            )
-        return np.array(
-            [
-                self.vectorizer.inverse_transform(document[document.any(1)])
-                for document in targets
-            ]
-        )
-
-    def inverse_transform_probabilities(self, targets):
-        """
-        Performs the inverse one-hot encoding for a prediction, given the matrix of probabilities.
-
-        Parameters
-        ----------
-        targets : The predicted probabilities for targets.
-
-        Returns
-        -------
-        List of decoded document tags
-        """
-        targets = np.array(
-            [document[np.std(document, axis=1) > 0.05] for document in targets]
-        )
-        n_class = targets.shape[2]
-        len_sentence = targets.shape[1]
-        num_sentences = targets.shape[0]
-        y_pred = np.array(
-            [
-                [
-                    1 if targets[0, i, j] == max(targets[0, i, :]) else 0
-                    for j in range(n_class)
-                ]
-                for i in range(len_sentence)
-            ]
-        )
-        return self.inverse_transform([y_pred])
-
-    def inverse_transform_probabilities(self, targets):
-        """
-        Performs the inverse one-hot encoding for a prediction, given the matrix of probabilities.
-        Parameters
-        ----------
-        targets : The predicted probabilities for targets.
-        Returns
-        -------
-        List of decoded document tags
-        """
-        targets = np.array(
-            [document[np.std(document, axis=1) > 0.05] for document in targets]
-        )
-        n_class = targets.shape[2]
-        len_sentence = targets.shape[1]
-        num_sentences = targets.shape[0]
-        y_pred = np.array(
-            [
-                [
-                    1 if targets[0, i, j] == max(targets[0, i, :]) else 0
-                    for j in range(n_class)
-                ]
-                for i in range(len_sentence)
-            ]
-        )
-        return self.inverse_transform([y_pred])
-
-    def get_classes(self):
-        """
-        Returns the classes of the target vectorizer.
-
-        Returns
-        -------
-        List of classes
-        """
-        return self.vectorizer.classes_
-
+def encode_target(target_series):
+    """
+    Encodes the target column of the dataset
+    """
+    return target_series.apply(lambda x: 1 if x == "SUPPORTS" else 0)
 
 if __name__ == "__main__":
     data = pd.read_csv('dataset/train_pairs.csv', index_col=0)
     tv = TextVectorizer()
     tv.adapt(data, ["Claim", "Evidence"])
-    data = tv.transform(data.iloc[:100], ["Claim", "Evidence"])
+    data = tv.transform(data, ["Claim", "Evidence"])
+    data.dropna(inplace=True)
+    X_train_claim = np.stack(data["Claim"])
+    X_train_evidence = np.stack(data["Evidence"])
+    y_train = data["Label"].to_numpy()
+    np.savetxt("X_train_claim.txt", X_train_claim)
+    np.savetxt("X_train_evidence.txt", X_train_evidence)
+    np.savetxt("y_train.txt", y_train)
+    
